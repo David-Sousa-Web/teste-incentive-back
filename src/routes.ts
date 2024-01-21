57fc83719841c000131d30f7e2f8b81192500665
David Sousa <david01sousa@hotmail.com>
2024-01-21 15:18:28 -0300

    feat: add user controller

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,11 @@ import { GetPayment } from "./controllers/payment-controller/get-payment-control
 import { CreatePayment } from "./controllers/payment-controller/create-payment-controller";
 import { DeletePayment } from "./controllers/payment-controller/delete-payment-controller";
 import { UpdatePayment } from "./controllers/payment-controller/update-payment-controller";
+import { CreateUser } from "./controllers/user-controller/create-user-controller";
 
 const routes = Router();
+
+routes.post("/user", CreateUser);
 
 routes.get("/payment", GetPayment);
 routes.post("/payment", CreatePayment);
