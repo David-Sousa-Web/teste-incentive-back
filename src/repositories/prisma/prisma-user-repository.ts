c49061d0e5504746ff752b556ec36d8d582bfae6
David Sousa <david01sousa@hotmail.com>
2024-01-21 15:11:57 -0300

    feat: create user use case

--- src/repositories/prisma/prisma-user-repository.ts
+++ src/repositories/prisma/prisma-user-repository.ts
@@ -3,6 +3,15 @@ import { UserRepository } from "../user-repository";
 import { prisma } from "../../lib/prisma";
 
 export class PrismaUserRepository implements UserRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
   async create(data: Prisma.UserUncheckedCreateInput) {
     const user = await prisma.user.create({
       data,
--- src/use-cases/users-use-case/create-user.ts
+++ src/use-cases/users-use-case/create-user.ts
@@ -1,10 +1,12 @@
 import { User } from "@prisma/client";
 import { UserRepository } from "../../repositories/user-repository";
+import { hash } from "bcryptjs";
+import { UserAlreadyExistError } from "../errors/user-already-exists";
 
 interface CreateUserUseCaseRequest {
   user: string;
   email: string;
-  senha: string;
+  password_hash: string;
 }
 
 interface CreateUserUseCaseResponse {
@@ -17,8 +19,16 @@ export class CreateUserUseCase {
   async execute({
     user,
     email,
-    senha,
+    password_hash,
   }: CreateUserUseCaseRequest): Promise<CreateUserUseCaseResponse> {
+    const senha = await hash(password_hash, 6);
+
+    const userWithSameEmail = await this.userRepository.findByEmail(email);
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistError();
+    }
+
     const users = await this.userRepository.create({
       user,
       email,
