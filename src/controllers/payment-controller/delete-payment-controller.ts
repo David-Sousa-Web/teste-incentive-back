f3609a2cfc8d2e3963e030f7d61a737509cbc67b
David Sousa <david01sousa@hotmail.com>
2024-01-21 03:33:09 -0300

    feat: create delete controller and use case

---
+++ src/controllers/payment-controller/delete-payment-controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { PrismaPaymentRepository } from "../../repositories/prisma/prisma-payment-repository";
+import { DeletePaymentUseCase } from "../../use-cases/delete-payments";
+
+export async function DeletePayment(req: Request, res: Response) {
+  const paymentID = req.params.paymentID;
+
+  try {
+    const paymentRepository = new PrismaPaymentRepository();
+    const paymentUseCase = new DeletePaymentUseCase(paymentRepository);
+
+    await paymentUseCase.execute({
+      paymentID,
+    });
+
+    res.status(200).json({ message: "Successfully Deleted Payment" });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
---
+++ src/use-cases/delete-payments.ts
@@ -0,0 +1,26 @@
+import { Pagamentos } from "@prisma/client";
+import { PaymentsRepository } from "../repositories/payments-repository";
+
+interface DeletePaymentUseCaseRequest {
+  paymentID: string;
+}
+
+interface DeletePaymentUseCaseResponse {
+  payment: Pagamentos;
+}
+
+export class DeletePaymentUseCase {
+  constructor(private PaymentsRepository: PaymentsRepository) {}
+
+  async execute({
+    paymentID,
+  }: DeletePaymentUseCaseRequest): Promise<DeletePaymentUseCaseResponse> {
+    const paymentDelete = await this.PaymentsRepository.deletePayments(
+      paymentID
+    );
+
+    return {
+      payment: paymentDelete,
+    };
+  }
+}
